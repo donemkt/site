8e2a6e57d4f46646cdcb686d07c916b74a2cfdb1
joaopedrosmelo <joaopedrosmelo@gmail.com>
2016-04-25 14:32:58 -0300

    alterar wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'done_db');
+define('DB_NAME', 'done_bd');
 
 /** MySQL database username */
-define('DB_USER', 'done_db');
+define('DB_USER', 'done_bd');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'DOneMkt@2016*bd');
